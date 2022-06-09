961bff81dc59cf110cd2f4cd19b8c8e2a92ad4de
Mezo <dominik@koch-bautechnik.de>
2022-06-09 18:07:30 +0200

    Bug fix

--- src/events/interactionCreate.js
+++ src/events/interactionCreate.js
@@ -2,35 +2,42 @@ const { readdirSync } = require('fs');
 const guildcreate = require('../util/Models/guildModel');
 
 module.exports = (interaction) => {
-  guildcreate.findOne({ guildID: interaction.guild.id }).then(async (result) => {
-    if (!result) {
-      await guildcreate.create({
-        guildID: interaction.guild.id,
-        language: 'en_EN',
-        botJoined: (Date.now() / 1000) | 0,
-      });
-    } else {
-    }
+  if (!interaction.guild) {
+    interaction.reply({
+      content: 'You need to be in a server to use this command.',
+      ephemeral: true,
+    });
+  } else {
+    guildcreate.findOne({ guildID: interaction.guild.id }).then(async (result) => {
+      if (!result) {
+        await guildcreate.create({
+          guildID: interaction.guild.id,
+          language: 'en_EN',
+          botJoined: (Date.now() / 1000) | 0,
+        });
+      } else {
+      }
 
-    const { client } = interaction;
-    const commandFiles = readdirSync('./src/commands/').filter((file) => file.endsWith('.js'));
-    const commands = [];
-    for (const file of commandFiles) {
-      const command = require(`../commands/${file}`);
-      commands.push(command.data.toJSON());
-      client.commands.set(command.data.name, command);
-    }
-    if (!interaction.isCommand()) return;
-    const command = client.commands.get(interaction.commandName);
-    if (!command) return;
-    try {
-      command.execute(interaction, client);
-    } catch (err) {
-      if (err) console.error(err);
-      interaction.reply({
-        content: 'An error occurred while executing that command.',
-        ephemeral: true,
-      });
-    }
-  });
+      const { client } = interaction;
+      const commandFiles = readdirSync('./src/commands/').filter((file) => file.endsWith('.js'));
+      const commands = [];
+      for (const file of commandFiles) {
+        const command = require(`../commands/${file}`);
+        commands.push(command.data.toJSON());
+        client.commands.set(command.data.name, command);
+      }
+      if (!interaction.isCommand()) return;
+      const command = client.commands.get(interaction.commandName);
+      if (!command) return;
+      try {
+        command.execute(interaction, client);
+      } catch (err) {
+        if (err) console.error(err);
+        interaction.reply({
+          content: 'An error occurred while executing that command.',
+          ephemeral: true,
+        });
+      }
+    });
+  }
 };
