8745937f7daea10cbca8383d1f091d97f8bb02b7
Mezo <dominik@koch-bautechnik.de>
2022-07-05 19:44:11 +0200

    Removed unused intents

--- src/index.ts
+++ src/index.ts
@@ -8,8 +8,7 @@ const client = new Client({
     intents: [
       Intents.FLAGS.GUILDS,
       Intents.FLAGS.GUILD_MEMBERS,
-      Intents.FLAGS.DIRECT_MESSAGES,
-      Intents.FLAGS.GUILD_MESSAGE_REACTIONS
+      Intents.FLAGS.DIRECT_MESSAGES
     ],
 });
 
