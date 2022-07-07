807289c308cb61886890317281de416eeb0a04a0
Mezo <dominik@koch-bautechnik.de>
2022-07-07 12:41:22 +0200

    Added join/leave logs

--- src/events/guildDelete.js
+++ src/events/guildDelete.js
@@ -1,30 +1,18 @@
 const { MessageEmbed } = require('discord.js');
 const config = require('../../config.json');
 
-const guildcreate = require('../util/Models/guildModel.ts');
-
-module.exports = (client, guild) => {
-  guildcreate.findOne({ guildID: guild.id }).then(async (result) => {
-    if (!result) {
-      await guildcreate.create({
-        guildID: guild.id,
-        language: 'en_EN',
-        botJoined: (Date.now() / 1000) | 0,
-      });
-    } else {
-    }
-  });
+module.exports = async (client, guild) => {
   const channel = client.channels.cache.get(config.logs);
   let own = await guild.fetchOwner();
   const embed = new MessageEmbed()
     .setThumbnail(guild.iconURL({ format: 'webp', dynamic: true, size: 1024 }))
-    .setTitle('Joined a Guild !!')
+    .setTitle('Left a Guild !!')
     .addField('Name: ', `${guild.name}`)
     .addField('ID:', ` ${guild.id}`)
     .addField('Owner: ', ` ${guild.members.cache.get(own.id) ? guild.members.cache.get(own.id).user.tag : 'Unknown user'} ${own.id} `)
     .addField('Member Count:', ` ${guild.memberCount} Members`)
-    .addField('Joined:', ` <t:${(Date.now() / 1000) | 0}:R>`)
-    .setColor('GREEN')
+    .addField('Left:', ` <t:${(Date.now() / 1000) | 0}:R>`)
+    .setColor('#ff0000')
     .addField(`${client.user.username}'s Server Count:`, `\`${client.guilds.cache.size}\` Severs`);
 
   await channel.send({ embeds: [embed] });
