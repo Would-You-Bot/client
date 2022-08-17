67be49256025431183904059068156762ac859f7
Mezo <dominik@koch-bautechnik.de>
2022-08-17 15:46:47 +0200

    custom and wwyd command

--- src/commands/custom.js
+++ src/commands/custom.js
@@ -4,7 +4,7 @@ const guildLang = require("../util/Models/guildModel.js");
 module.exports = {
   data: new SlashCommandBuilder()
     .setName("custom")
-    .setDescription("custom")
+    .setDescription("Send a custom would you message")
     .addStringOption((option) => option.setName("custom").setDescription("Custom what would you do.").setRequired(true)),
 
   /**
@@ -12,98 +12,27 @@ module.exports = {
    * @param {Client} client
    */
   async execute(interaction, client) {
-    let wouldyouembed;
     guildLang
       .findOne({ guildID: interaction.guild.id })
       .then(async (result) => {
-        const { WouldYou } =
+        const { Custom } =
           await require(`../languages/${result.language}.json`);
-        const { Useless_Superpowers, Useful_Superpowers } =
-          await require(`../data/superpower-${result.language}.json`);
 
-          wouldyouembed = new EmbedBuilder()
-          .setTitle("Would you want this superpower?")
+          let customembed = new EmbedBuilder()
+          .setTitle(Custom.embed.title)
           .setDescription(`> ${interaction.options.get("custom").value}`)
           .setColor("#0598F6")
-          .setFooter({ text: `${WouldYou.embed.footer}` })
+          .setFooter({ text: `${Custom.embed.footer}` })
           .setTimestamp()
-        //   .setDescription(`Would you ${interaction.options.get("custom").value}?`);
-        //   .addFields({
-        //     name: WouldYou.embed.Usefulname,
-        //     value: `> ${superpower}`,
-        //     inline: false,
-        //   });
         try  {
         const message = await interaction.reply({
-          embeds: [wouldyouembed],
+          embeds: [customembed],
           fetchReply: true,
         });
 
         try {
           await message.react("✅");
           await message.react("❌");
-        //   const filter = (reaction) => {
-        //     return reaction.emoji.name == "✅" || reaction.emoji.name == "❌";
-        //   };
-
-        //   const collector = message.createReactionCollector({filter, time: 20000});
-        //   collector.on("collect", async () => {
-        //   });
-
-        //   collector.on("end", async () => {
-
-        //     let totalreactions = message.reactions.cache.get('✅').count -1 + message.reactions.cache.get('❌').count - 1;
-        //     let percentage = Math.round(((message.reactions.cache.get('✅').count - 1) / (totalreactions)) * 100)
-        //     let emoji = null;
-        //     let color = null;
-        //     let userstotal = (totalreactions < 2) ? `${WouldYou.stats.user}` : `${WouldYou.stats.users}`;
-
-
-        //     if (message.reactions.cache.get('✅').count -1 + message.reactions.cache.get('❌').count - 1 == 0) {
-        //       percentage = 0;
-        //       emoji = "🤷";
-        //       color = "#F0F0F0";
-        //     }
-
-        //     if(percentage > 50) {
-        //       color = "#0598F6"
-        //       emoji = '✅'
-        //     } else if(percentage < 50) {
-        //       color = "#F00505"
-        //       emoji = '❌'
-        //     } else {
-        //       color = "#F0F0F0"
-        //       emoji = '🤷'
-        //     }
-
-        //     wouldyouembed = new EmbedBuilder()
-        //     .setColor(color)
-        //     .setFooter({ text: `${WouldYou.embed.footer}` })
-        //     .setTimestamp()
-        //     // {
-        //     //     name: WouldYou.embed.Uselessname,
-        //     //     value: `> ${superpower}`,
-        //     //     inline: false,
-        //     //   }, 
-        //     .addFields(
-        //     {
-        //       name: "Stats",
-        //       value: `> **${percentage}%** ${WouldYou.stats.of} **${totalreactions} ${userstotal}** ${WouldYou.stats.taking} ${emoji}`,
-        //     }
-        //     );
-
-        //     try {
-        //         await message.reactions.removeAll()
-        //     } catch (error) {
-        //         console.log(error)
-        //     }
-
-        //     await interaction.editReply({
-        //       embeds: [wouldyouembed],
-        //     });
-
-        //     collector.stop();
-        //   });
         } catch (error) {}
       } catch (error) {}
       });
---
+++ src/commands/wwyd.js
@@ -0,0 +1,41 @@
+const { EmbedBuilder, SlashCommandBuilder } = require('discord.js');
+const guildLang = require('../util/Models/guildModel.js');
+
+module.exports = {
+  data: new SlashCommandBuilder()
+    .setName('wwyd')
+    .setDescription('commanddescription'),
+        
+  /**
+   * @param {CommandInteraction} interaction
+   * @param {Client} client
+   */
+
+
+  async execute(interaction, client) {
+    guildLang
+      .findOne({ guildID: interaction.guild.id })
+      .then(async (result) => {
+        const { Wwyd } = require(`../languages/${result.language}.json`);
+
+        const { WhatYouDo } = require(`../data/wwyd-${result.language}.json`);
+
+        let 
+        wwydstring = WhatYouDo[
+          Math.floor(Math.random() * WhatYouDo.length)
+        ]
+
+            let wwydembed = new EmbedBuilder()
+
+            .setColor('#0598F6')
+            .setFooter({ text: `${Wwyd.embed.footer}` })
+            .setTimestamp()
+            .setTitle(Wwyd.embed.title)
+            .setDescription("> " + wwydstring)
+
+        await interaction.reply({ embeds: [wwydembed] });
+
+
+      });
+  },
+};
