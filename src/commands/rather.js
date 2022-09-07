2861f8dce456c7fa21539149fd3ee79b254d0f79
ForGetFulSkyBro <forgetfulskybro9@gmail.com>
2022-09-07 16:51:14 -0400

    Fixed rather command and some buttons

--- src/commands/rather.js
+++ src/commands/rather.js
@@ -196,12 +196,24 @@ module.exports = {
 
           case 'useless':
             {
-              let uselesspower1 = Useless_Powers[
-                Math.floor(Math.random() * Useless_Powers.length)
-              ];
-              let uselesspower2 = Useless_Powers[
-                Math.floor(Math.random() * Useless_Powers.length)
-              ];
+              let uselesspower1;
+              let uselesspower2;
+              if (result.customTypes === "regular") {
+                uselesspower1 = Useful_Powers[Math.floor(Math.random() * Useful_Powers.length)];
+                uselesspower2 = Useful_Powers[Math.floor(Math.random() * Useful_Powers.length)];
+              } else if (result.customTypes === "mixed") {
+                if (result.customMessages.length > 0) {
+                  uselesspower1 = result.customMessages.filter(c => c.type === "useless")[Math.floor(Math.random() * result.customMessages.filter(c => c.type === "useless").length)].msg
+                } else {
+                  return await interaction.reply({ ephemeral: true, content: "There's currently no custom WouldYou messages to be displayed! Either make some or change the type using \`/wytype <type>\`" })
+                }
+                uselesspower2 = Useful_Powers[Math.floor(Math.random() * Useful_Powers.length)];
+              } else if (result.customTypes === "custom") {
+                if (result.customMessages.length === 0) return await interaction.reply({ ephemeral: true, content: "There's currently no custom WouldYou messages to be displayed! Either make some or change the type using \`/wytype <type>\`" })
+                uselesspower1 = result.customMessages.filter(c => c.type === "useless")[Math.floor(Math.random() * result.customMessages.filter(c => c.type === "useless").length)].msg;
+                uselesspower2 = result.customMessages.filter(c => c.type === "useless")[Math.floor(Math.random() * result.customMessages.filter(c => c.type === "useless").length)].msg;
+              }
+
 
               let ratherembed = new EmbedBuilder()
                 .setColor('#F00505')
