1abde4ab7615a5b17845444ffae9e1be1c31154b
Dominik K <dominik@koch-bautechnik.de>
2022-09-13 20:14:57 +0200

    Fix rather.js
    Still missing the buttons!

--- src/commands/rather.js
+++ src/commands/rather.js
@@ -12,16 +12,22 @@ module.exports = {
     .setDescription('Get a would you rather question.')
     .addSubcommand((subcommand) => subcommand
       .setName('useful')
-      .setDescription('Get a useful would you rather')
+      .setDescription('Get a useful would you rather question')
       .addBooleanOption((option) => option
         .setName('voting')
         .setDescription('Do you want the users to be able to vote?')))
     .addSubcommand((subcommand) => subcommand
       .setName('useless')
-      .setDescription('Get a useless would you rather')
+      .setDescription('Get a useless would you rather question')
       .addBooleanOption((option) => option
         .setName('voting')
-        .setDescription('Do you want the users to be able to vote?'))),
+        .setDescription('Do you want the users to be able to vote?')))
+    .addSubcommand((subcommand) => subcommand
+    .setName('nsfw')
+    .setDescription('Get a borderline nsfw would you rather question')
+    .addBooleanOption((option) => option
+      .setName('voting')
+      .setDescription('Do you want the users to be able to vote?'))),
 
   /**
    * @param {CommandInteraction} interaction
@@ -35,7 +41,7 @@ module.exports = {
         let voting = true;
         if (interaction.options.getBoolean('voting') == false) voting = false;
         const { Rather } = await require(`../languages/${result.language}.json`);
-        const { Useless_Powers, Useful_Powers } = await require(`../data/power-${result.language}.json`);
+        const { Useless_Powers, Useful_Powers, Nsfw } = await require(`../data/power-${result.language}.json`);
 
         const button = new ActionRowBuilder().addComponents(
           new ButtonBuilder()
@@ -66,14 +72,15 @@ module.exports = {
                 usefulpower1 = Useful_Powers[Math.floor(Math.random() * Useful_Powers.length)];
                 usefulpower2 = Useful_Powers[Math.floor(Math.random() * Useful_Powers.length)];
               } else if (result.customTypes === "mixed") {
-                if (result.customMessages.length > 0) {
-                  usefulpower1 = result.customMessages.filter(c => c.type === "useful")[Math.floor(Math.random() * result.customMessages.filter(c => c.type === "useful").length)].msg
+                if (result.customMessages.filter(c => c.type === "useful") != 0) {
+                  usefulpower1 = result.customMessages.filter(c => c.type === "useful")[Math.floor(Math.random() * result.customMessages.filter(c => c.type === "useful").length)].msg || Useful_Powers[Math.floor(Math.random() * Useful_Powers.length)];
                 } else {
-                  return await interaction.reply({ ephemeral: true, content: "There's currently no custom WouldYou messages to be displayed! Either make some or change the type using \`/wytype <type>\`" })
+                  usefulpower1 = Useful_Powers[Math.floor(Math.random() * Useful_Powers.length)];
+                  usefulpower2 = Useful_Powers[Math.floor(Math.random() * Useful_Powers.length)];
                 }
                 usefulpower2 = Useful_Powers[Math.floor(Math.random() * Useful_Powers.length)];
               } else if (result.customTypes === "custom") {
-                if (result.customMessages.length === 0) return await interaction.reply({ ephemeral: true, content: "There's currently no custom WouldYou messages to be displayed! Either make some or change the type using \`/wytype <type>\`" })
+                if (result.customMessages.filter(c => c.type === "useful") == 0) return await interaction.reply({ ephemeral: true, content: "There's currently no custom WouldYou messages to be displayed! Either make some or change the type using \`/wytype <type>\`" })
                 usefulpower1 = result.customMessages.filter(c => c.type === "useful")[Math.floor(Math.random() * result.customMessages.filter(c => c.type === "useful").length)].msg;
                 usefulpower2 = result.customMessages.filter(c => c.type === "useful")[Math.floor(Math.random() * result.customMessages.filter(c => c.type === "useful").length)].msg;
               }
@@ -199,17 +206,18 @@ module.exports = {
               let uselesspower1;
               let uselesspower2;
               if (result.customTypes === "regular") {
-                uselesspower1 = Useful_Powers[Math.floor(Math.random() * Useful_Powers.length)];
-                uselesspower2 = Useful_Powers[Math.floor(Math.random() * Useful_Powers.length)];
+                uselesspower1 = Useless_Powers[Math.floor(Math.random() * Useless_Powers.length)];
+                uselesspower2 = Useless_Powers[Math.floor(Math.random() * Useless_Powers.length)];
               } else if (result.customTypes === "mixed") {
-                if (result.customMessages.length > 0) {
-                  uselesspower1 = result.customMessages.filter(c => c.type === "useless")[Math.floor(Math.random() * result.customMessages.filter(c => c.type === "useless").length)].msg
+                if (result.customMessages.filter(c => c.type === "useless") != 0) {
+                  uselesspower1 = result.customMessages.filter(c => c.type === "useless")[Math.floor(Math.random() * result.customMessages.filter(c => c.type === "useless").length)].msg || Useless_Powers[Math.floor(Math.random() * Useless_Powers.length)];
                 } else {
-                  return await interaction.reply({ ephemeral: true, content: "There's currently no custom WouldYou messages to be displayed! Either make some or change the type using \`/wytype <type>\`" })
+                uselesspower1 = Useless_Powers[Math.floor(Math.random() * Useless_Powers.length)];
+                uselesspower2 = Useless_Powers[Math.floor(Math.random() * Useless_Powers.length)];
                 }
-                uselesspower2 = Useful_Powers[Math.floor(Math.random() * Useful_Powers.length)];
+                uselesspower2 = Useless_Powers[Math.floor(Math.random() * Useless_Powers.length)];
               } else if (result.customTypes === "custom") {
-                if (result.customMessages.length === 0) return await interaction.reply({ ephemeral: true, content: "There's currently no custom WouldYou messages to be displayed! Either make some or change the type using \`/wytype <type>\`" })
+                if (result.customMessages.filter(c => c.type === "useless") == 0) return await interaction.reply({ ephemeral: true, content: "There's currently no custom WouldYou messages to be displayed! Either make some or change the type using \`/wytype <type>\`" })
                 uselesspower1 = result.customMessages.filter(c => c.type === "useless")[Math.floor(Math.random() * result.customMessages.filter(c => c.type === "useless").length)].msg;
                 uselesspower2 = result.customMessages.filter(c => c.type === "useless")[Math.floor(Math.random() * result.customMessages.filter(c => c.type === "useless").length)].msg;
               }
@@ -330,7 +338,145 @@ module.exports = {
               }
             }
             break;
-        }
+
+        case 'nsfw':
+          {
+            let nsfwpower1;
+            let nsfwpower2;
+            if (result.customTypes === "regular") {
+              nsfwpower1 = Nsfw[Math.floor(Math.random() * Nsfw.length)];
+              nsfwpower2 = Nsfw[Math.floor(Math.random() * Nsfw.length)];
+            } else if (result.customTypes === "mixed") {
+              if (result.customMessages.filter(c => c.type === "nsfw") != 0) {
+                nsfwpower1 = result.customMessages.filter(c => c.type === "nsfw")[Math.floor(Math.random() * result.customMessages.filter(c => c.type === "nsfw").length)].msg || Nsfw[Math.floor(Math.random() * Nsfw.length)]
+              } else {
+                nsfwpower1 = Nsfw[Math.floor(Math.random() * Nsfw.length)];
+                nsfwpower2 = Nsfw[Math.floor(Math.random() * Nsfw.length)];
+              }
+              nsfwpower2 = Nsfw[Math.floor(Math.random() * Nsfw.length)];
+            } else if (result.customTypes === "custom") {
+              if (result.customMessages.filter(c => c.type === "nsfw") == 0) return await interaction.reply({ ephemeral: true, content: "There's currently no custom WouldYou messages to be displayed! Either make some or change the type using \`/wytype <type>\`" })
+              nsfwpower1 = result.customMessages.filter(c => c.type === "nsfw")[Math.floor(Math.random() * result.customMessages.filter(c => c.type === "nsfw").length)].msg;
+              nsfwpower2 = result.customMessages.filter(c => c.type === "nsfw")[Math.floor(Math.random() * result.customMessages.filter(c => c.type === "nsfw").length)].msg;
+            }
+
+
+            let ratherembed = new EmbedBuilder()
+              .setColor('#F00505')
+              .addFields(
+                {
+                  name: Rather.embed.uselessname,
+                  value: `> 1️⃣ ${nsfwpower1}`,
+                  inline: false,
+                },
+                {
+                  name: Rather.embed.uselessname2,
+                  value: `> 2️⃣ ${nsfwpower2}`,
+                  inline: false,
+                },
+              )
+              .setFooter({
+                text: `${Rather.embed.footer}`,
+                iconURL: client.user.avatarURL(),
+              })
+              .setTimestamp();
+
+            let message = await interaction
+              .reply({
+                embeds: [ratherembed],
+                fetchReply: true,
+                components: rbutton || [],
+              })
+              .catch((err) => {
+                return;
+              });
+            if (interaction.options.getBoolean('voting') == false) {
+            } else {
+              try {
+                await message.react('1️⃣');
+                await message.react('2️⃣');
+                const filter = (reaction) => reaction.emoji.name == '1️⃣' || reaction.emoji.name == '2️⃣';
+
+                const collector = message.createReactionCollector({
+                  filter,
+                  time: 20000,
+                });
+                collector.on('collect', async () => {});
+
+                collector.on('end', async () => {
+                  if (
+                    message.reactions.cache.get('1️⃣').count - 1
+                    > message.reactions.cache.get('2️⃣').count - 1
+                  ) {
+                    ratherembed = new EmbedBuilder()
+                      .setColor('#F00505')
+                      .setFooter({
+                        text: `${Rather.embed.footer}`,
+                        iconURL: client.user.avatarURL(),
+                      })
+                      .setTimestamp()
+                      .addFields({
+                        name: Rather.embed.thispower,
+                        value: `> 1️⃣ ${nsfwpower1}`,
+                        inline: false,
+                      });
+                  } else if (
+                    message.reactions.cache.get('1️⃣').count - 1
+                    < message.reactions.cache.get('2️⃣').count - 1
+                  ) {
+                    ratherembed = new EmbedBuilder()
+                      .setColor('#F00505')
+                      .setFooter({
+                        text: `${Rather.embed.footer}`,
+                        iconURL: client.user.avatarURL(),
+                      })
+                      .setTimestamp()
+                      .addFields({
+                        name: Rather.embed.thispower,
+                        value: `> 2️⃣ ${nsfwpower2}`,
+                        inline: false,
+                      });
+                  } else {
+                    ratherembed = new EmbedBuilder()
+                      .setColor('#ffffff')
+                      .addFields(
+                        {
+                          name: Rather.embed.uselessname,
+                          value: `> 1️⃣ ${nsfwpower1}`,
+                          inline: false,
+                        },
+                        {
+                          name: Rather.embed.uselessname2,
+                          value: `> 2️⃣ ${nsfwpower2}`,
+                          inline: false,
+                        },
+                      )
+                      .setFooter({
+                        text: `${Rather.embed.footer}`,
+                        iconURL: client.user.avatarURL(),
+                      })
+                      .setTimestamp();
+                  }
+
+                  try {
+                    await message.reactions.removeAll();
+                  } catch (error) {}
+                  await interaction
+                    .editReply({
+                      embeds: [ratherembed],
+                      components: rbutton || [],
+                    })
+                    .catch((err) => {
+                      return;
+                    });
+
+                  collector.stop();
+                });
+              } catch (error) {}
+            }
+          }
+          break;
+      }
       });
   },
 };
