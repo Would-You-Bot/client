7d432acc909eee1b7b4768106e320d6cdc03fdec
ForGetFulSkyBro <47897305+forgetfulskybro@users.noreply.github.com>
2022-10-20 16:30:21 -0400

    Update guildDelete.js

--- src/events/guildDelete.js
+++ src/events/guildDelete.js
@@ -5,8 +5,8 @@ module.exports = async (client, guild) => {
   const webhookClient = new WebhookClient({ url: process.env.WEBHOOK });
 
   webhookClient.send({
-    content: `🔴 Left ${guild.name}. I'm now in ${client.guilds.cache.size} guilds.`,
-    username: `${guild.name}`,
+    content: `<:BadCheck:1025490660968628436> Left ${guild.name}. I'm now in ${client.guilds.cache.size} guilds.`,
+    username: `${guild.name.replace("Discord", "").replace("discord", "")}`,
     avatarURL: guild.iconURL({ format: 'webp', dynamic: true, size: 1024 }),
   }).catch((err) => console.log(err));
 };
