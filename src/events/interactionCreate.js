1317de92980fdfde5f9c8d33ce697853f0b3f4c3
ForGetFulSkyBro <forgetfulskybro9@gmail.com>
2022-11-06 13:26:20 -0500

    Fixed interactionCreate

--- src/events/interactionCreate.js
+++ src/events/interactionCreate.js
@@ -1,5 +1,5 @@
 const guildcreate = require('../util/Models/guildModel');
-
+let lang;
 module.exports = (client, interaction) => {
   if (!interaction.guild) {
     interaction.reply({
@@ -16,8 +16,10 @@ module.exports = (client, interaction) => {
             language: 'en_EN',
             botJoined: Date.now() / 1000 | 0,
           });
-        } 
-        const { inter } = require(`../languages/${result.language}.json`);
+          lang = require(`../languages/en_EN.json`);
+        } else {
+          lang = require(`../languages/${result.language}.json`);
+        }
         if (interaction.isChatInputCommand()) {
           const command = client.commands.get(interaction.commandName);
           if (!command) return;
@@ -26,14 +28,14 @@ module.exports = (client, interaction) => {
           } catch (err) {
             if (err) console.error(err);
             interaction.reply({
-              content: inter.error,
+              content: lang.inter.error,
               ephemeral: true,
             });
           }
         } else if (interaction.isButton()) {
-          if (client.used.has(interaction.user.id)) return await interaction.reply({ ephemeral: true, content: inter.wait }).catch(() => { });
+          if (client.used.has(interaction.user.id)) return await interaction.reply({ ephemeral: true, content: lang.inter.wait }).catch(() => { });
           const button = client.buttons.get(interaction.customId);
-          if (!button) return interaction.reply({ content: inter.again, ephemeral: true }).catch(() => {  });
+          if (!button) return interaction.reply({ content: lang.inter.again, ephemeral: true }).catch(() => {  });
           try {
             client.used.set(interaction.user.id, Date.now() + 30000)
             setTimeout(() => client.used.delete(interaction.user.id), 30000)
@@ -42,7 +44,7 @@ module.exports = (client, interaction) => {
           } catch (err) {
             if (err) console.error(err);
             interaction.reply({
-              content: inter.error,
+              content: lang.inter.error,
               ephemeral: true,
             });
           }
