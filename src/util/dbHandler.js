09fca658b151027816b520f363150ae9c1d68e41
ForGetFulSkyBro <47897305+forgetfulskybro@users.noreply.github.com>
2022-11-11 17:14:51 -0500

    Update dbHandler.js

--- src/util/dbHandler.js
+++ src/util/dbHandler.js
@@ -3,11 +3,11 @@ require('dotenv').config();
 
 const { ChalkAdvanced } = require('chalk-advanced');
 
-
-connect(process.env.MONGO_URI, {	connect(process.env.MONGO_URI, {
-  useNewUrlParser: true,	  useNewUrlParser: true,
-  serverSelectionTimeoutMS: 1000	}).catch((err) => { console.log(err) }).then(() => console.log(
-}).then(() => console.log(	
-  `${ChalkAdvanced.white('Database')} ${ChalkAdvanced.gray(	  `${ChalkAdvanced.white('Database')} ${ChalkAdvanced.gray(
-    '>',	    '>',
-  )} ${ChalkAdvanced.green('Successfully loaded database')}`,	  )} ${ChalkAdvanced.green('Successfully loaded database')}`,
+connect(process.env.MONGO_URI, {
+  useNewUrlParser: true,
+  serverSelectionTimeoutMS: 1000
+}).then(() => console.log(
+  `${ChalkAdvanced.white('Database')} ${ChalkAdvanced.gray(
+    '>',
+  )} ${ChalkAdvanced.green('Successfully loaded database')}`,
+));
