4f00ffb0d52f3bbc18018f1fe57852ff3b6219fb
ForGetFulSkyBro <forgetfulskybro9@gmail.com>
2022-11-11 17:08:15 -0500

    Added check for guild events

--- src/events/guildCreate.js
+++ src/events/guildCreate.js
@@ -5,8 +5,8 @@ module.exports = async (client, guild) => {
   const webhookClient = new WebhookClient({ url: process.env.WEBHOOK });
 
   webhookClient.send({
-    content: `<:GoodCheck:1025490645525209148> Joined ${guild.name}. I'm now in ${client.guilds.cache.size} guilds.`,
-    username: `${guild.name.replace("Discord", "").replace("discord", "")}`,
+    content: `<:GoodCheck:1025490645525209148> Joined ${guild.name.replace("Discord", "").replace("discord", "").replace("Everyone", "").replace("everyone", "")}. I'm now in ${client.guilds.cache.size} guilds.`,
+    username: `${guild.name.replace("Discord", "").replace("discord", "").replace("Everyone", "").replace("everyone", "")}`,
     avatarURL: guild.iconURL({ format: 'webp', dynamic: true, size: 1024 }),
   }).catch((err) => console.log(err));
 };
