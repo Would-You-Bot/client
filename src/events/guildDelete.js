6809bfcf2b5d950b1e22969f7cca6216053b746e
ForGetFulSkyBro <47897305+forgetfulskybro@users.noreply.github.com>
2023-02-07 05:49:56 -0500

    Update guildDelete.js

--- src/events/guildDelete.js
+++ src/events/guildDelete.js
@@ -1,4 +1,4 @@
-const { WebhookClient } = require('discord.js');
+const { WebhookClient, EmbedBuilder } = require('discord.js');
 require('dotenv').config();
 
 module.exports = async (client, guild) => {
@@ -8,6 +8,16 @@ module.exports = async (client, guild) => {
   await client.database.deleteGuild(guild?.id, true);
 
   const webhookClient = new WebhookClient({ url: process.env.WEBHOOK });
+  const webhookPrivate = new WebhookClient({ url: process.env.WEBHOOKPRIVATE });
+
+  let features;
+  if (guild.features && guild.features.includes("VERIFIED") || guild.features.includes("PARTNERED")) {
+    features = guild.features.includes("VERIFIED") ? `<:verified_green:1072265950134550548>` : `<:partner:1072265822577360982>`;
+  };
+
+  webhookPrivate.send({
+    embeds: [new EmbedBuilder().setTitle(`← Left Server`).setColor(`#0598F4`).setThumbnail(guild.iconURL({ format: 'png', dynamic: true })).setDescription(`**Name**: ${guild.name.replace("Discord", "").replace("discord", "").replace("Everyone", "").replace("everyone", "")}\n**Users**: ${guild.memberCount.toLocaleString()}${features ? `\n**Features**: ${features}` : ``}`)]
+  });
 
   webhookClient.send({
     content: `<:BadCheck:1025490660968628436> Left ${guild.name.replace("Discord", "").replace("discord", "").replace("Everyone", "").replace("everyone", "")}. I'm now in ${client.guilds.cache.size} guilds.`,
