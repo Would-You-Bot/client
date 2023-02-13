310cb47d58a1cff994e118ef4d22555b19eeb36a
Dominik K <dominik@koch-bautechnik.de>
2023-02-13 19:41:07 +0100

    Fix bug + language support

--- src/commands/canvas.js
+++ src/commands/canvas.js
@@ -3,6 +3,7 @@ const {
     ActionRowBuilder,
     ButtonBuilder,
     SlashCommandBuilder,
+    AttachmentBuilder
 } = require('discord.js');
 const guildModel = require('../util/Models/guildModel');
 
@@ -23,63 +24,34 @@ module.exports = {
      * @param {guildModel} guildDb
      */
     async execute(interaction, client, guildDb) {
+
         const Canvas = require("@napi-rs/canvas");
-        const canvas = Canvas.createCanvas(800, 600);
+        const canvas = Canvas.createCanvas(600, 300);
         const ctx = canvas.getContext("2d");
-        
-        // Set background color
-        ctx.fillStyle = "lightgray";
-        ctx.fillRect(0, 0, canvas.width, canvas.height);
-        
-        // Draw title
-        ctx.font = "48px sans-serif";
-        ctx.fillStyle = "black";
-        ctx.textAlign = "center";
-        ctx.fillText("Would You Rather?", canvas.width / 2, 50);
-        
-        // Draw first question
-        ctx.font = "36px sans-serif";
-        ctx.fillStyle = "blue";
-        ctx.textAlign = "left";
-        ctx.fillText("Option A", 50, 150);
-        
-        // Draw second question
-        ctx.font = "36px sans-serif";
-        ctx.fillStyle = "red";
-        ctx.textAlign = "right";
-        ctx.fillText("Option B", canvas.width - 50, 150);
-        
-        // Draw separator line
-        ctx.beginPath();
-        ctx.moveTo(0, 100);
-        ctx.lineTo(canvas.width, 100);
-        ctx.lineWidth = 2;
-        ctx.strokeStyle = "black";
-        ctx.stroke();
-        
-        // Draw circles around the questions
-        ctx.beginPath();
-        ctx.arc(50, 150, 40, 0, 2 * Math.PI);
-        ctx.fillStyle = "blue";
-        ctx.fill();
-        ctx.strokeStyle = "black";
-        ctx.lineWidth = 2;
-        ctx.stroke();
-        
-        ctx.beginPath();
-        ctx.arc(canvas.width - 50, 150, 40, 0, 2 * Math.PI);
-        ctx.fillStyle = "red";
-        ctx.fill();
-        ctx.strokeStyle = "black";
-        ctx.lineWidth = 2;
-        ctx.stroke();
-        
-        const fs = require("fs");
-        const out = fs.createWriteStream(__dirname + "/would-you-rather.png");
-        const stream = canvas.toBuffer("image/png");
-        out.write(stream);
-        out.end();
-        
-      
-    },
+
+        const image = await Canvas.loadImage("./src/data/images/template.png");
+        await ctx.drawImage(image, 0, 0, 600, 300);
+
+        if (guildDb.language === "de_DE") {
+            const detranslation = await Canvas.loadImage("./src/data/images/rather-de.png")
+            ctx.drawImage(detranslation, 0, 0, 600, 300);
+        } else if (guildDb.language === "en_EN") {
+            const entranslation = await Canvas.loadImage("./src/data/images/rather-en.png")
+            ctx.drawImage(entranslation, 0, 0, 600, 300);
+        } else if (guildDb.language === "es_ES") {
+            const estranslation = await Canvas.loadImage("./src/data/images/rather-es.png")
+            ctx.drawImage(estranslation, 0, 0, 600, 300);
+        }
+
+        attachment = new AttachmentBuilder(
+            await canvas.encode("png"),
+            {
+                name: "spotify-card.png",
+            })
+        interaction
+            .reply({
+                files: [attachment],
+            })
+            .catch((err) => console.log(err));
+    }
 };
--- src/commands/help.js
+++ src/commands/help.js
@@ -27,7 +27,7 @@ module.exports = {
         let type;
         if (guildDb.language === "de_DE") {
             type = "de"
-        } else if (guildDb.language === "en_US") {
+        } else if (guildDb.language === "en_EN") {
             type = "en"
         } else if (guildDb.language === "es_ES") {
             type = "es"
