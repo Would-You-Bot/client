836ee8b636fd8287db3d12243c8edceff7d3d494
Sean <sattler.junior@gmail.com>
2023-02-26 22:11:17 +0100

    Added it to .filter() should be better

--- src/util/dailyMessage.js
+++ src/util/dailyMessage.js
@@ -5,7 +5,7 @@ const CronJob = require('cron').CronJob;
 
 module.exports = class DailyMessage {
     constructor(c) {
-       this.c = c;
+        this.c = c;
     }
 
     /**
@@ -26,6 +26,7 @@ module.exports = class DailyMessage {
         let test = 0;
         let guilds = await this.c.database.getAll();
         guilds = guilds.filter(g => this.c.guilds.cache.has(g.guildID) && g.dailyMsg);
+        guilds = guilds.filter(g => mom.tz(g.dailyTimezone).format("HH:mm") === "12:00");
 
         console.log(
             `${ChalkAdvanced.white('Daily Message')} ${ChalkAdvanced.gray(
@@ -35,99 +36,28 @@ module.exports = class DailyMessage {
 
         let i = 0;
         for (const db of guilds) {
-            if(!db?.dailyChannel) continue;
+            if (!db?.dailyChannel) continue;
             if (!db.dailyMsg) continue;
-            if (mom.tz(db.dailyTimezone).format("HH:mm") === "12:00") {
-                i++;
-                setTimeout(async () => {
-                    const channel = await this.c.channels.fetch(db.dailyChannel).catch(err => { console.log(err)
-                    });
+            i++;
+            setTimeout(async () => {
+                const channel = await this.c.channels.fetch(db.dailyChannel).catch(err => {
+                    console.log(err)
+                });
 
-                    if (!channel?.id) return; // Always directly return before do to many actions
+                if (!channel?.id) return; // Always directly return before do to many actions
 
-                    const {Useless_Powers, Useful_Powers} = await require(`../data/power-${db.language}.json`);
-                    const {WouldYou, Rather} = await require(`../languages/${db.language}.json`);
+                const {Useless_Powers, Useful_Powers} = await require(`../data/power-${db.language}.json`);
+                const {WouldYou, Rather} = await require(`../languages/${db.language}.json`);
 
-                    if (db.dailyRather) {
-                        let power;
-                        let power2;
-                        if (db.customTypes === "regular") {
-                            let array = [];
-                            array.push(Useful_Powers[Math.floor(Math.random() * Useful_Powers.length)]);
-                            array.push(Useless_Powers[Math.floor(Math.random() * Useless_Powers.length)]);
-                            power = array[Math.floor(Math.random() * array.length)];
-                            power2 = array[Math.floor(Math.random() * array.length)];
-                            array = [];
-                        } else if (db.customTypes === "mixed") {
-                            let array = [];
-                            if (db.customMessages.filter(c => c.type !== "nsfw") != 0) {
-                                array.push(db.customMessages.filter(c => c.type !== "nsfw")[Math.floor(Math.random() * db.customMessages.filter(c => c.type !== "nsfw").length)].msg);
-                            } else {
-                                power = Useful_Powers[Math.floor(Math.random() * Useful_Powers.length)];
-                            }
-                            array.push(Useful_Powers[Math.floor(Math.random() * Useful_Powers.length)]);
-                            array.push(Useless_Powers[Math.floor(Math.random() * Useless_Powers.length)]);
-                            power = array[Math.floor(Math.random() * array.length)];
-                            power2 = array[Math.floor(Math.random() * array.length)];
-                            array = [];
-                        } else if (db.customTypes === "custom") {
-                            if (db.customMessages.filter(c => c.type !== "nsfw") === 0) {
-                                test++
-                                console.log("before 1")
-                                this.c.webhookHandler.sendWebhook(
-                                    channel,
-                                    db.dailyChannel,
-                                    {
-                                        content: 'There\'s currently no custom Would You messages to be displayed for daily messages! Either make new ones or turn off daily messages.'
-                                    }
-                                ).catch(err => { console.log(err)
-                                });
-                                console.log("after 1")
-                            }
-
-                            power = db.customMessages.filter(c => c.type !== "nsfw")[Math.floor(Math.random() * db.customMessages.filter(c => c.type !== "nsfw").length)].msg;
-                            power2 = db.customMessages.filter(c => c.type !== "nsfw")[Math.floor(Math.random() * db.customMessages.filter(c => c.type !== "nsfw").length)].msg;
-                        }
-
-                        const embed = new EmbedBuilder()
-                            .setColor('#0598F6')
-                            .setFooter({
-                                text: `${Rather.embed.footer}`,
-                                iconURL: this.c.user.avatarURL(),
-                            })
-                            .setTimestamp()
-                            .addFields(
-                                {
-                                    name: Rather.embed.usefulname,
-                                    value: `> ${power}`,
-                                    inline: false,
-                                },
-                                {
-                                    name: Rather.embed.usefulname2,
-                                    value: `> ${power2}`,
-                                    inline: false,
-                                },
-                            )
-                        test++
-                        console.log("before 2")
-                        this.c.webhookHandler.sendWebhook(
-                            channel,
-                            db.dailyChannel,
-                            {
-                                embeds: [embed],
-                                content: db.dailyRole ? `<@&${db.dailyRole}>` : null
-                            }
-                        ).catch(err => { console.log(err)
-                        });
-                        console.log("after 1")
-                    }
-
+                if (db.dailyRather) {
                     let power;
+                    let power2;
                     if (db.customTypes === "regular") {
                         let array = [];
                         array.push(Useful_Powers[Math.floor(Math.random() * Useful_Powers.length)]);
                         array.push(Useless_Powers[Math.floor(Math.random() * Useless_Powers.length)]);
-                        power = array[Math.floor(Math.random() * array.length)]
+                        power = array[Math.floor(Math.random() * array.length)];
+                        power2 = array[Math.floor(Math.random() * array.length)];
                         array = [];
                     } else if (db.customTypes === "mixed") {
                         let array = [];
@@ -138,39 +68,50 @@ module.exports = class DailyMessage {
                         }
                         array.push(Useful_Powers[Math.floor(Math.random() * Useful_Powers.length)]);
                         array.push(Useless_Powers[Math.floor(Math.random() * Useless_Powers.length)]);
-                        power = array[Math.floor(Math.random() * array.length)]
+                        power = array[Math.floor(Math.random() * array.length)];
+                        power2 = array[Math.floor(Math.random() * array.length)];
                         array = [];
                     } else if (db.customTypes === "custom") {
                         if (db.customMessages.filter(c => c.type !== "nsfw") === 0) {
                             test++
-                            console.log("before 3")
+                            console.log("before 1")
                             this.c.webhookHandler.sendWebhook(
                                 channel,
                                 db.dailyChannel,
                                 {
                                     content: 'There\'s currently no custom Would You messages to be displayed for daily messages! Either make new ones or turn off daily messages.'
                                 }
-                            ).catch(err => { console.log(err)});
-                            console.log("after 3")
+                            ).catch(err => {
+                                console.log(err)
+                            });
+                            console.log("after 1")
                         }
 
                         power = db.customMessages.filter(c => c.type !== "nsfw")[Math.floor(Math.random() * db.customMessages.filter(c => c.type !== "nsfw").length)].msg;
+                        power2 = db.customMessages.filter(c => c.type !== "nsfw")[Math.floor(Math.random() * db.customMessages.filter(c => c.type !== "nsfw").length)].msg;
                     }
 
                     const embed = new EmbedBuilder()
                         .setColor('#0598F6')
                         .setFooter({
-                            text: `${WouldYou.embed.footer}`,
+                            text: `${Rather.embed.footer}`,
                             iconURL: this.c.user.avatarURL(),
                         })
                         .setTimestamp()
-                        .addFields({
-                            name: WouldYou.embed.Usefulname,
-                            value: `> ${power}`,
-                            inline: false,
-                        });
+                        .addFields(
+                            {
+                                name: Rather.embed.usefulname,
+                                value: `> ${power}`,
+                                inline: false,
+                            },
+                            {
+                                name: Rather.embed.usefulname2,
+                                value: `> ${power2}`,
+                                inline: false,
+                            },
+                        )
                     test++
-                    console.log("before 4")
+                    console.log("before 2")
                     this.c.webhookHandler.sendWebhook(
                         channel,
                         db.dailyChannel,
@@ -178,11 +119,75 @@ module.exports = class DailyMessage {
                             embeds: [embed],
                             content: db.dailyRole ? `<@&${db.dailyRole}>` : null
                         }
-                    ).catch(err => { console.log(err)
+                    ).catch(err => {
+                        console.log(err)
                     });
-                    console.log("after 4")
-                }, i * 2500) // We do a little timeout here to work against discord ratelimit with 50reqs/second
-            }
+                    console.log("after 1")
+                }
+
+                let power;
+                if (db.customTypes === "regular") {
+                    let array = [];
+                    array.push(Useful_Powers[Math.floor(Math.random() * Useful_Powers.length)]);
+                    array.push(Useless_Powers[Math.floor(Math.random() * Useless_Powers.length)]);
+                    power = array[Math.floor(Math.random() * array.length)]
+                    array = [];
+                } else if (db.customTypes === "mixed") {
+                    let array = [];
+                    if (db.customMessages.filter(c => c.type !== "nsfw") != 0) {
+                        array.push(db.customMessages.filter(c => c.type !== "nsfw")[Math.floor(Math.random() * db.customMessages.filter(c => c.type !== "nsfw").length)].msg);
+                    } else {
+                        power = Useful_Powers[Math.floor(Math.random() * Useful_Powers.length)];
+                    }
+                    array.push(Useful_Powers[Math.floor(Math.random() * Useful_Powers.length)]);
+                    array.push(Useless_Powers[Math.floor(Math.random() * Useless_Powers.length)]);
+                    power = array[Math.floor(Math.random() * array.length)]
+                    array = [];
+                } else if (db.customTypes === "custom") {
+                    if (db.customMessages.filter(c => c.type !== "nsfw") === 0) {
+                        test++
+                        console.log("before 3")
+                        this.c.webhookHandler.sendWebhook(
+                            channel,
+                            db.dailyChannel,
+                            {
+                                content: 'There\'s currently no custom Would You messages to be displayed for daily messages! Either make new ones or turn off daily messages.'
+                            }
+                        ).catch(err => {
+                            console.log(err)
+                        });
+                        console.log("after 3")
+                    }
+
+                    power = db.customMessages.filter(c => c.type !== "nsfw")[Math.floor(Math.random() * db.customMessages.filter(c => c.type !== "nsfw").length)].msg;
+                }
+
+                const embed = new EmbedBuilder()
+                    .setColor('#0598F6')
+                    .setFooter({
+                        text: `${WouldYou.embed.footer}`,
+                        iconURL: this.c.user.avatarURL(),
+                    })
+                    .setTimestamp()
+                    .addFields({
+                        name: WouldYou.embed.Usefulname,
+                        value: `> ${power}`,
+                        inline: false,
+                    });
+                test++
+                console.log("before 4")
+                this.c.webhookHandler.sendWebhook(
+                    channel,
+                    db.dailyChannel,
+                    {
+                        embeds: [embed],
+                        content: db.dailyRole ? `<@&${db.dailyRole}>` : null
+                    }
+                ).catch(err => {
+                    console.log(err)
+                });
+                console.log("after 4")
+            }, i * 2500) // We do a little timeout here to work against discord ratelimit with 50reqs/second
         }
         console.log(test)
     }
