9cc4969e29f79912b8be288560bb94db0e233a32
DanPlayz <damuffin36@gmail.com>
2023-04-08 12:49:29 -0700

    fix a blaring issue with mentions

--- src/events/guildCreate.js
+++ src/events/guildCreate.js
@@ -25,20 +25,22 @@ module.exports = async (client, guild) => {
                     format: 'png',
                     dynamic: true
                 }))
-                .setDescription(`**Name**: ${guild.name.replace("Discord", "").replace("discord", "").replace("Everyone", "").replace("everyone", "")}\n**Users**: ${guild.memberCount.toLocaleString()}${features ? `\n**Features**: ${features}` : ``}`)
+                .setDescription(`**Name**: ${guild.name}\n**Users**: ${guild.memberCount.toLocaleString()}${features ? `\n**Features**: ${features}` : ``}`)
                 .setFooter({
                     text: global?.devBot ? 'Dev Bot' : 'Main Bot',
                 })
-        ]
+        ],
+        allowedMentions: { parse: [] }
     })
 
     if (!global?.devBot) {
         const webhookClient = new WebhookClient({ url: process.env.WEBHOOK });
 
         await webhookClient.send({
-            content: `<:GoodCheck:1025490645525209148> Joined ${guild.name.replace("Discord", "").replace("discord", "").replace("Everyone", "").replace("everyone", "")}. I'm now in ${client.guilds.cache.size} guilds.`,
+            content: `<:GoodCheck:1025490645525209148> Joined ${guild.name}. I'm now in ${client.guilds.cache.size} guilds.`,
             username: `${guild.name.replace("Discord", "").replace("discord", "").replace("Everyone", "").replace("everyone", "")}`,
             avatarURL: guild.iconURL({format: 'webp', dynamic: true, size: 1024}),
+            allowedMentions: { parse: [] }
         }).catch((err) => console.log(err));
     }
 };
