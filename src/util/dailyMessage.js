7e1231273b23d8c32b680426525cf03c07ed20be
Dominik K <dominik@koch-bautechnik.de>
2023-04-15 22:47:12 +0200

    Update dailymsg

--- src/util/dailyMessage.js
+++ src/util/dailyMessage.js
@@ -23,8 +23,8 @@ module.exports = class DailyMessage {
      */
     async runSchedule() {
         let guilds = await this.client.database.getAll();
-        guilds = guilds.filter(g => this.client.guilds.cache.has(g.guildID) && g.dailyMsg);
-        //guilds = guilds.filter(g => mom.tz(g.dailyTimezone).format("HH:mm") === g.dailyInterval);
+        //guilds = guilds.filter(g => this.client.guilds.cache.has(g.guildID) && g.dailyMsg);
+        guilds = guilds.filter(g => mom.tz(g.dailyTimezone).format("HH:mm") === g.dailyInterval);
 
         console.log(
             `${ChalkAdvanced.white('Daily Message')} ${ChalkAdvanced.gray(
