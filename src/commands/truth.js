5e34ce1b25d28f8322376f7f1c856bcd79902ae1
Dominik K <dominik@koch-bautechnik.de>
2023-05-04 21:30:03 +0200

    Add truth or dare template

---
+++ src/commands/truth.js
@@ -0,0 +1,22 @@
+const { EmbedBuilder, SlashCommandBuilder } = require('discord.js');
+
+const { version } = require('../../package.json');
+
+module.exports = {
+  data: new SlashCommandBuilder()
+    .setName('truth')
+    .setDescription('Shows information about the bot.')
+    .setDMPermission(false)
+    .setDescriptionLocalizations({
+      de: 'Zeigt einige Informationen über den Bot.',
+      "es-ES": 'Muestra información sobre el bot.'
+    }),
+  /**
+   * @param {CommandInteraction} interaction
+   * @param {WouldYou} client
+   * @param {guildModel} guildDb
+   */
+  async execute(interaction, client, guildDb) {
+   
+  },
+};
