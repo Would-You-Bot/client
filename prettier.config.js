fa03f62a800ac3cb6d772b91026052681080c381
slekup <74949101+slekup@users.noreply.github.com>
2023-05-11 12:01:39 +1200

    prettier and normalize endings config

---
+++ prettier.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  trailingComma: 'es5',
+  tabWidth: 2,
+  useTabs: false,
+  semi: true,
+  singleQuote: true,
+  bracketSpacing: true,
+  arrowParens: 'always',
+  jsxSingleQuote: false,
+  printwidth: 120,
+};
