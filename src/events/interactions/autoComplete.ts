7e05a979404e04af8129a260ae55a466db93d295
slekup <74949101+slekup@users.noreply.github.com>
2023-05-20 15:11:33 +1200

    interaction events, types

---
+++ src/events/interactions/autoComplete.ts
@@ -0,0 +1,35 @@
+import { AutocompleteInteraction, Events } from 'discord.js';
+
+import { CoreEvent } from '@typings/core';
+import { ExtendedClient } from 'src/client';
+
+const event: CoreEvent<ExtendedClient, [AutocompleteInteraction]> = {
+  name: Events.InteractionCreate,
+  /**
+   * Execute the auto complete event handler.
+   * @param client The extended client.
+   * @param interaction The autocomplete interaction.
+   * @returns A promise.
+   */
+  async execute(client: ExtendedClient, interaction: AutocompleteInteraction): Promise<unknown> {
+    if (!client.synced) return;
+    if (!interaction.isAutocomplete()) return;
+
+    // Get the command
+    const command = client.commands.get(interaction.commandName);
+
+    // If the command could not be found
+    if (!command) {
+      client.logger.error(`No command matching ${interaction.commandName} was found.`);
+      return;
+    }
+
+    try {
+      await command.autocomplete(interaction, client);
+    } catch (error) {
+      client.logger.error(error);
+    }
+  },
+};
+
+export default event;
