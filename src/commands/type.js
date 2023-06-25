41bc2127439ccfe89074464caf05f555763242a2
Dominik K <mezo.dsn@gmail.com>
2023-06-25 21:16:25 +0200

    feat: type command (Unfinished)

---
+++ src/commands/type.js
@@ -0,0 +1,85 @@
+const {
+    EmbedBuilder,
+    SlashCommandBuilder,
+    PermissionFlagsBits,
+} = require('discord.js');
+const guildModel = require('../util/Models/guildModel');
+
+module.exports = {
+    requireGuild: true,
+    data: new SlashCommandBuilder()
+        .setName('type')
+        .setDescription('Changes the type of messages that will be used.')
+        .setDMPermission(false)
+        .setDescriptionLocalizations({
+            de: 'Ändert den Typ der Nachrichten, die verwendet werden.',
+            "es-ES": 'Cambia el tipo de mensajes que se utilizarán.',
+        })
+        .addSubcommand((subcommand) => subcommand.setName('regular').setDescription('This changes it to use only default messages.'))
+        .addSubcommand((subcommand) => subcommand.setName('mixed').setDescription('This changes it to use both custom & default messages.'))
+        .addSubcommand((subcommand) => subcommand.setName('custom').setDescription('This changes it to use only custom messages.')),
+
+    /**
+     * @param {CommandInteraction} interaction
+     * @param {WouldYou} client
+     * @param {guildModel} guildDb
+     */
+
+    async execute(interaction, client, guildDb) {
+        let typeEmbed;
+
+        if (
+            interaction.member.permissions.has(PermissionFlagsBits.ManageGuild)
+            || global.checkDebug(guildDb, interaction?.user?.id)
+        ) {
+            switch (interaction.options.getSubcommand()) {
+                case 'regular':
+                    await client.database.updateGuild(interaction.guildId, {
+                        customTypes: 'regular',
+                    }, true);
+
+                    typeEmbed = new EmbedBuilder()
+                        .setTitle(client.translation.get(guildDb?.language, "wyType.embed.title"))
+                        .setDescription(client.translation.get(guildDb?.language, "wyType.embed.descDef"));
+                    break;
+                case 'mixed':
+                    await client.database.updateGuild(interaction.guildId, {
+                        customTypes: 'mixed',
+                    }, true);
+
+                    typeEmbed = new EmbedBuilder()
+                        .setTitle(client.translation.get(guildDb?.language, "wyType.embed.title"))
+                        .setDescription(client.translation.get(guildDb?.language, "wyType.embed.descBoth"));
+                    break;
+                case 'custom':
+                    await client.database.updateGuild(interaction.guildId, {
+                        customTypes: 'custom',
+                    }, true);
+
+                    typeEmbed = new EmbedBuilder()
+                        .setTitle(client.translation.get(guildDb?.language, "wyType.embed.title"))
+                        .setDescription(client.translation.get(guildDb?.language, "wyType.embed.descCust"));
+                    break;
+            }
+
+            return interaction.reply({
+                embeds: [typeEmbed],
+                ephemeral: true,
+            }).catch((err) => {
+                console.log(err);
+            });
+        } else {
+            const errorembed = new EmbedBuilder()
+                .setColor('#F00505')
+                .setTitle('Error!')
+                .setDescription(client.translation.get(guildDb?.language, "Settings.embed.error"));
+
+            return interaction.reply({
+                embeds: [errorembed],
+                ephemeral: true,
+            }).catch((err) => {
+                console.log(err);
+            });
+        }
+    },
+};
