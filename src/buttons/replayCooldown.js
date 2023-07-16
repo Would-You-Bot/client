b67a80b95700e48460efbe6984a969530672db24
ForGetFulSkyBro <47897305+forgetfulskybro@users.noreply.github.com>
2023-07-16 10:22:30 -0400

    Update replayCooldown.js

--- src/buttons/replayCooldown.js
+++ src/buttons/replayCooldown.js
@@ -41,6 +41,12 @@ module.exports = {
           ephemeral: true,
           content: client.translation.get(guildDb?.language, 'Settings.replaySame'),
         });
+
+        if (value < 2000) return modalInteraction.reply({
+          ephemeral: true,
+          content: client.translation.get(guildDb?.language, 'Settings.cooldownMin'),
+        });
+
         if (isNumericRegex(value) === false)
         return modalInteraction.reply({
           ephemeral: true,
@@ -50,11 +56,7 @@ module.exports = {
         const generalMsg = new EmbedBuilder()
           .setTitle(client.translation.get(guildDb?.language, 'Settings.embed.generalTitle'))
           .setDescription(
-            `${client.translation.get(guildDb?.language, 'Settings.embed.voteCooldown')}: ${
-              guildDb.voteCooldown
-                ? `${guildDb.voteCooldown}`
-                : `<:x_:1077962443013238814>`
-            }\n${client.translation.get(guildDb?.language, 'Settings.embed.replayCooldown')}: ${
+            `${client.translation.get(guildDb?.language, 'Settings.embed.replayType')}: ${guildDb.replayType}\n${client.translation.get(guildDb?.language, 'Settings.embed.replayCooldown')}: ${
               guildDb.replayCooldown
                 ? `${value}`
                 : `<:x_:1077962443013238814>`
@@ -68,13 +70,14 @@ module.exports = {
 
         const generalButtons = new ActionRowBuilder().addComponents(
           new ButtonBuilder()
-            .setCustomId("voteCooldown")
-            .setLabel(client.translation.get(guildDb?.language, 'Settings.button.voteCooldown'))
-            .setStyle(guildDb.voteCooldown ? "Success" : "Secondary"),
-          new ButtonBuilder()
             .setCustomId("replayCooldown")
             .setLabel(client.translation.get(guildDb?.language, 'Settings.button.replayCooldown'))
-            .setStyle(guildDb.replayCooldown ? "Success" : "Secondary")
+            .setStyle(guildDb.replayCooldown ? "Success" : "Secondary"),
+          new ButtonBuilder()
+            .setCustomId("replayType")
+            .setLabel(client.translation.get(guildDb?.language, 'Settings.button.replayType'))
+            .setStyle("Primary")
+            .setEmoji("📝"),
         );
 
         await client.database.updateGuild(interaction.guild.id, {
