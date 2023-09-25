ffaffc2876ed9b1f1952a00e35256433f49cfade
mezotv <mezotv@users.noreply.github.com>
2023-09-25 01:28:03 +0000

    chore(style): format files

--- src/events/interactionCreate.js
+++ src/events/interactionCreate.js
@@ -1,6 +1,5 @@
 const userModel = require("../util/Models/userModel");
 module.exports = async (client, interaction) => {
-
   const user = await userModel.findOne({ userID: interaction.user.id });
 
   const restrict = [
@@ -54,27 +53,27 @@ module.exports = async (client, interaction) => {
     // const { inter } = require(`../languages/${guildDb.language || "en_EN"}.json`);
     if (interaction.isChatInputCommand()) {
       const statsMap = {
-        "wouldyourather": "wouldyou.used.command",
-        "neverhaveiever": "neverhaveiever.used.command",
-        "higherlower": "higherlower.used.command",
-        "wwyd": "whatwouldyoudo.used.command",
+        wouldyourather: "wouldyou.used.command",
+        neverhaveiever: "neverhaveiever.used.command",
+        higherlower: "higherlower.used.command",
+        wwyd: "whatwouldyoudo.used.command",
       };
 
       if (!user) {
         await userModel.create({
           userID: interaction.user.id,
-          [statsMap[interaction.commandName]]: 1
+          [statsMap[interaction.commandName]]: 1,
         });
       } else {
         // Get the field path based on the command name
         const fieldPath = statsMap[interaction.commandName];
-        console.log(fieldPath)
+        console.log(fieldPath);
         if (fieldPath) {
           // Increment the specified field using $inc
           await userModel.updateOne(
             { userID: interaction.user.id }, // Specify the query to find the user
             { $inc: { [fieldPath]: 1 } }, // Use computed fieldPath
-            { upsert: true }
+            { upsert: true },
           );
         }
       }
@@ -94,58 +93,61 @@ module.exports = async (client, interaction) => {
       let button = client.buttons.get(interaction.customId);
 
       const customId = interaction.customId.split("_");
-      
+
       if (customId[3]) {
-        if (customId[2] === "neverhaveiever" || customId[2] === "wouldyourather") {
+        if (
+          customId[2] === "neverhaveiever" ||
+          customId[2] === "wouldyourather"
+        ) {
           const action = customId[3] == 0 ? "yes" : "no";
           const commandName = customId[2];
           const fieldName = `${commandName}.${action}`;
-          console.log(fieldName)
+          console.log(fieldName);
           if (!user) {
             // If the user doesn't exist, create a new one with the userID
 
-            await userModel.create(            { userID: interaction.user.id }, // Specify the query to find the user
-            { $inc: { [fieldName]: 1 } }, // Use computed fieldPath
-            { upsert: true })
+            await userModel.create(
+              { userID: interaction.user.id }, // Specify the query to find the user
+              { $inc: { [fieldName]: 1 } }, // Use computed fieldPath
+              { upsert: true },
+            );
           } else {
             // Increment the specified field using $inc
             await userModel.updateOne(
-              { userID: interaction.user.id},
-              { $inc: { [fieldName]: 1 }},
-              { upsert: true }
+              { userID: interaction.user.id },
+              { $inc: { [fieldName]: 1 } },
+              { upsert: true },
             );
           }
         }
       }
-      
-      
-   
-        const replyMap = {
-          "wouldyourather": "wouldyou.used.replay",
-          "neverhaveiever": "neverhaveiever.used.replay",
-          "higherlower": "higherlower.used.replay",
-          "wwyd": "whatwouldyoudo.used.replay",
-        };
-  
-        if (!user) {
-          await userModel.create({
-            userID: interaction.user.id,
-            [replyMap[interaction.customId]]: 1
-          });
-        } else {
-          // Get the field path based on the command name
-          const fieldPath = replyMap[interaction.commandName];
-        
-          if (fieldPath) {
-            // Increment the specified field using $inc
-            await userModel.updateOne(
-              { userID: interaction.user.id }, // Specify the query to find the user
-              { $inc: { [fieldPath]: 1 } }, // Use computed fieldPath
-              { upsert: true }
-            );
-          }
+
+      const replyMap = {
+        wouldyourather: "wouldyou.used.replay",
+        neverhaveiever: "neverhaveiever.used.replay",
+        higherlower: "higherlower.used.replay",
+        wwyd: "whatwouldyoudo.used.replay",
+      };
+
+      if (!user) {
+        await userModel.create({
+          userID: interaction.user.id,
+          [replyMap[interaction.customId]]: 1,
+        });
+      } else {
+        // Get the field path based on the command name
+        const fieldPath = replyMap[interaction.commandName];
+
+        if (fieldPath) {
+          // Increment the specified field using $inc
+          await userModel.updateOne(
+            { userID: interaction.user.id }, // Specify the query to find the user
+            { $inc: { [fieldPath]: 1 } }, // Use computed fieldPath
+            { upsert: true },
+          );
         }
-      
+      }
+
       if (interaction.customId.startsWith("voting_"))
         button = client.buttons.get("voting");
       if (interaction.customId.startsWith("result_"))
