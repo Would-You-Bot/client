267ba834adb061edc4022e11b77712ae1d740091
Dominik K <mezo.dsn@gmail.com>
2023-10-15 12:20:59 +0200

    add: privacy description

--- src/commands/privacy.js
+++ src/commands/privacy.js
@@ -1,28 +1,29 @@
 const { SlashCommandBuilder } = require("discord.js");
 module.exports = {
-  data: new SlashCommandBuilder()
-    .setName("privacy")
-    .setDescription("Privacy settings")
-    .setDMPermission(false)
-    .setDescriptionLocalizations({
-      de: "Lorem ipsum",
-      "es-ES": "Lorem ipsum",
-      fr: "Lorem ipsum",
-    })
-    .addStringOption((option) =>
-      option
-        .setName("votes")
-        .setDescription(
-          "Select if you want your name to be mentioned in results of votes.",
-        )
-        .setRequired(true)
-        .addChoices({ name: "On", value: "On" }, { name: "Off", value: "Off" }),
-    ),
-  /**
-   * @param {CommandInteraction} interaction
-   * @param {WouldYou} client
-   * @param {guildModel} guildDb
-   */
+    data: new SlashCommandBuilder()
+        .setName("privacy")
+        .setDescription("Change the users privacy settings.")
+        .setDMPermission(false)
+        .setDescriptionLocalizations({
+            de: "Ändere die Datenschutzeinstellungen des Nutzers.",
+            "es-ES": "Ajusta la configuración de privacidad del usuario.",
+            fr: "Modifiez les paramètres de confidentialité de l'utilisateur.",
+        })
+        .addStringOption((option) =>
+            option
+                .setName("votes")
+                .setDescription("Select if you want your name to be mentioned in results of votes.")
+                .setRequired(true)
+                .addChoices(
+                    { name: "On", value: "On" },
+                    { name: "Off", value: "Off" },
+                ),
+        ),
+    /**
+     * @param {CommandInteraction} interaction
+     * @param {WouldYou} client
+     * @param {guildModel} guildDb
+     */
 
   async execute(interaction, client, guildDb) {
     const choice = interaction.options.get("votes");
