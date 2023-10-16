f730857a23089d7c42a520ac5acef0ea926b1fda
mezotv <mezotv@users.noreply.github.com>
2023-10-16 01:29:10 +0000

    chore(style): format files

--- src/commands/privacy.js
+++ src/commands/privacy.js
@@ -1,29 +1,28 @@
 const { SlashCommandBuilder } = require("discord.js");
 module.exports = {
-    data: new SlashCommandBuilder()
-        .setName("privacy")
-        .setDescription("Change the users privacy settings.")
-        .setDMPermission(false)
-        .setDescriptionLocalizations({
-            de: "Ändere die Datenschutzeinstellungen des Nutzers.",
-            "es-ES": "Ajusta la configuración de privacidad del usuario.",
-            fr: "Modifiez les paramètres de confidentialité de l'utilisateur.",
-        })
-        .addStringOption((option) =>
-            option
-                .setName("votes")
-                .setDescription("Select if you want your name to be mentioned in results of votes.")
-                .setRequired(true)
-                .addChoices(
-                    { name: "On", value: "On" },
-                    { name: "Off", value: "Off" },
-                ),
-        ),
-    /**
-     * @param {CommandInteraction} interaction
-     * @param {WouldYou} client
-     * @param {guildModel} guildDb
-     */
+  data: new SlashCommandBuilder()
+    .setName("privacy")
+    .setDescription("Change the users privacy settings.")
+    .setDMPermission(false)
+    .setDescriptionLocalizations({
+      de: "Ändere die Datenschutzeinstellungen des Nutzers.",
+      "es-ES": "Ajusta la configuración de privacidad del usuario.",
+      fr: "Modifiez les paramètres de confidentialité de l'utilisateur.",
+    })
+    .addStringOption((option) =>
+      option
+        .setName("votes")
+        .setDescription(
+          "Select if you want your name to be mentioned in results of votes.",
+        )
+        .setRequired(true)
+        .addChoices({ name: "On", value: "On" }, { name: "Off", value: "Off" }),
+    ),
+  /**
+   * @param {CommandInteraction} interaction
+   * @param {WouldYou} client
+   * @param {guildModel} guildDb
+   */
 
   async execute(interaction, client, guildDb) {
     const choice = interaction.options.get("votes");
