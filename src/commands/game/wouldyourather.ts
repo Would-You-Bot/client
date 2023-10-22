003beba1f3574bebebc5653a36b7a379ceafc0bc
Dominik K <mezo.dsn@gmail.com>
2023-10-22 13:43:22 +0200

    feat(wyr): add ai generated questions

--- src/commands/game/wouldyourather.ts
+++ src/commands/game/wouldyourather.ts
@@ -5,10 +5,17 @@ import {
   ButtonBuilder,
   MessageActionRowComponentBuilder,
 } from "discord.js";
+import dotenv from "dotenv";
+dotenv.config();
 import shuffle from "../../util/shuffle";
 import { captureException } from "@sentry/node";
 import { ChatInputCommand } from "../../models";
 import { getWouldYouRather } from "../../util/Functions/jsonImport";
+import OpenAI from "openai";
+
+const openai = new OpenAI({
+  apiKey: process.env.OPENAI_TOKEN as string,
+});
 
 const command: ChatInputCommand = {
   requireGuild: true,
@@ -20,7 +27,19 @@ const command: ChatInputCommand = {
       de: "Erhalte eine Würdest du eher Frage.",
       "es-ES": "Obtiene une pregunta ¿Qué prefieres?",
       fr: "Obtenez une question préférez-vous.",
-    }),
+    })
+    .addStringOption((option) =>
+      option
+        .setName("prompt")
+        .setDescription("Enter a custom prompt to generate a question.")
+        .setDescriptionLocalizations({
+          de: "Geben Sie einen benutzerdefinierten Prompt ein, um eine Frage zu generieren.",
+          "es-ES":
+            "Ingrese un indicador personalizado para generar una pregunta.",
+          fr: "Entrez un indicateur personnalisé pour générer une question.",
+        })
+        .setRequired(false)
+    ),
 
   /**
    * @param {CommandInteraction} interaction
@@ -29,10 +48,27 @@ const command: ChatInputCommand = {
    */
 
   execute: async (interaction, client, guildDb) => {
-    var General = await getWouldYouRather(guildDb.language);
+    let AI = "";
+    if (interaction.options.getString("prompt")) {
+      const chatCompletion = await openai.chat.completions.create({
+        messages: [
+          {
+            role: "user",
+            content:
+              "Generate a would you rather questions that is 100% safe for work and pg-13 with the following theme: " +
+              interaction.options.getString("prompt") +
+              "make sure anything that is slightly not safe for work gets filtered out and replaced by a nice and pg 3 topic",
+          },
+        ],
+        model: process.env.OPENAI_MODEL as string,
+      });
+      AI = chatCompletion.choices[0].message.content as string;
+    }
+
+    let General = await getWouldYouRather(guildDb.language);
 
     const dbquestions = guildDb.customMessages.filter(
-      (c) => c.type !== "nsfw" && c.type === "wouldyourather",
+      (c) => c.type !== "nsfw" && c.type === "wouldyourather"
     );
 
     let wouldyourather = [] as string[];
@@ -54,14 +90,17 @@ const command: ChatInputCommand = {
         break;
     }
     const Random = Math.floor(Math.random() * wouldyourather.length);
-
     let ratherembed = new EmbedBuilder()
       .setColor("#0598F6")
       .setFooter({
-        text: `Requested by ${interaction.user.username} | Type: General | ID: ${Random}`,
+        text: `Requested by ${interaction.user.username} | Type: ${
+          interaction.options.getString("prompt")
+            ? "AI Generated"
+            : "General"
+        } | ID: ${interaction.options.getString("prompt") ? "gpt-3.5-turbo" : Random}`,
         iconURL: interaction.user.avatarURL() || undefined,
       })
-      .setDescription(wouldyourather[Random] || "Unknown");
+      .setDescription(`${interaction.options.getString("prompt") ? AI : wouldyourather[Random]}`);
 
     const mainRow = new ActionRowBuilder<MessageActionRowComponentBuilder>();
     if (Math.round(Math.random() * 15) < 3) {
@@ -71,7 +110,7 @@ const command: ChatInputCommand = {
           .setStyle(5)
           .setEmoji("1009964111045607525")
           .setURL(
-            "https://discord.com/oauth2/authorize?client_id=981649513427111957&permissions=275415247936&scope=bot%20applications.commands",
+            "https://discord.com/oauth2/authorize?client_id=981649513427111957&permissions=275415247936&scope=bot%20applications.commands"
           ),
       ]);
     }
@@ -93,7 +132,7 @@ const command: ChatInputCommand = {
       time < three_minutes
         ? new Date(0)
         : new Date(~~((Date.now() + time) / 1000)),
-      "wouldyourather",
+      "wouldyourather"
     );
 
     await interaction
