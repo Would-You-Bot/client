6cf42adcbfb4a3eefe80d28fd7e2e3118e781dc5
Gersti <patrick.gerstacker@gmail.com>
2023-10-24 10:48:07 +0200

    fix: corrected uptime timestamp calculation

--- src/commands/utility/info.ts
+++ src/commands/utility/info.ts
@@ -21,8 +21,7 @@ const command: ChatInputCommand = {
    */
   execute: async (interaction, client, guildDb) => {
     const unixstamp =
-      Math.floor((Date.now() / 1000) | 0) -
-      Math.floor(client.uptime || 0 / 1000);
+      Math.floor(Date.now() / 1000) - Math.floor((client.uptime || 0) / 1000);
 
     function round(num: number) {
       const m = Number((Math.abs(num) * 100).toPrecision(15));
@@ -35,32 +34,32 @@ const command: ChatInputCommand = {
     console.log(
       `Guild Members: ${client.guilds.cache.reduce(
         (acc, guild) => acc + guild.members.cache.size,
-        0,
-      )}`,
+        0
+      )}`
     );
     console.log(`Channels: ${client.channels.cache.size}`);
     console.log(
       `Roles: ${client.guilds.cache.reduce(
         (acc, guild) => acc + guild.roles.cache.size,
-        0,
-      )}`,
+        0
+      )}`
     );
     console.log(
       `VoiceStates: ${client.guilds.cache.reduce(
         (acc, guild) => acc + guild.voiceStates.cache.size,
-        0,
-      )}`,
+        0
+      )}`
     );
     console.log(
       `Presences: ${client.guilds.cache.reduce(
         (acc, guild) => acc + guild.presences.cache.size,
-        0,
-      )}`,
+        0
+      )}`
     );
     console.log(
       `RAM Usage: ${(process.memoryUsage().heapUsed / 1024 / 1024).toFixed(
-        0,
-      )}/${(process.memoryUsage().rss / 1024 / 1024).toFixed(0)}MB`,
+        0
+      )}/${(process.memoryUsage().rss / 1024 / 1024).toFixed(0)}MB`
     );
     console.log("\n");
 
@@ -89,7 +88,7 @@ const command: ChatInputCommand = {
         {
           name: "Memory 🎇",
           value: `\`\`\`${round(
-            process.memoryUsage().heapUsed / 1000000000,
+            process.memoryUsage().heapUsed / 1000000000
           )}GB\n\`\`\``,
           inline: true,
         },
@@ -103,7 +102,7 @@ const command: ChatInputCommand = {
           name: "Bot Version 🧾",
           value: `\`\`\`v${version}\`\`\``,
           inline: true,
-        },
+        }
       )
       .setThumbnail(client.user?.displayAvatarURL() || null)
       .setFooter({
