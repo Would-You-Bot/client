ca30327cbe527d16a183ecdcd7e759e366440c67
Modlin <132472711+modlin-dev@users.noreply.github.com>
2023-11-30 10:25:40 +0600

    TypeScript Test Support

--- src/cluster.ts
+++ src/cluster.ts
@@ -5,28 +5,32 @@ import {
   HeartbeatManager,
 } from "discord-hybrid-sharding";
 import { white, gray, green } from "chalk-advanced";
+import path from "path";
 
-const manager = new ClusterManager(`${__dirname}/index.js`, {
-  totalShards: "auto",
-  shardsPerClusters: 10,
-  mode: "process",
-  token: process.env.TOKEN,
-});
+const manager = new ClusterManager(
+  `${__dirname}/index${path.extname(__filename)}`,
+  {
+    totalShards: "auto",
+    shardsPerClusters: 10,
+    mode: "process",
+    token: process.env.TOKEN,
+  }
+);
 
 manager.extend(
   new ReClusterManager(),
   new HeartbeatManager({
     interval: 10000,
     maxMissedHeartbeats: 10,
-  }),
+  })
 );
 
-manager.on("clusterCreate", (cluster) => {
+manager.on("clusterCreate", (cluster) =>
   console.log(
     `${white("Would You?")} ${gray(">")} ${green(
-      "Successfully created cluster #" + cluster.id,
-    )}`,
-  );
-});
+      "Successfully created cluster #" + cluster.id
+    )}`
+  )
+);
 
 manager.spawn({ timeout: -1 });
