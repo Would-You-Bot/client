d033b2fe345d771d7e493277891fe42f948bccd2
Dominik K <mezo.dsn@gmail.com>
2023-12-04 18:21:06 +0100

    Revert "fix: custom.ts"
    This reverts commit 97295cf62e970637cdf09d6165fc9200898f4fc0.

--- src/commands/settings/custom.ts
+++ src/commands/settings/custom.ts
@@ -170,7 +170,7 @@ const command: ChatInputCommand = {
             .setColor("#0598F4")
             .setDescription(
               `${
-                generativeText?.value === false
+                generativeText.value === false
                   ? `${client.translation.get(
                       guildDb?.language,
                       "wyCustom.success.embedAdd.descAccept",
@@ -206,7 +206,7 @@ const command: ChatInputCommand = {
             )
             .setFooter({
               text: `Would You ${
-                !generativeText.value
+                generativeText.value === false
                   ? `| ${client.translation.get(
                       guildDb?.language,
                       "wyCustom.success.embedAdd.footerDisable",
@@ -254,7 +254,7 @@ const command: ChatInputCommand = {
 
           interaction
             .reply(
-              !generativeText.value
+              generativeText.value === false
                 ? {
                     embeds: [typeEmbed],
                     components: [add],
@@ -267,7 +267,10 @@ const command: ChatInputCommand = {
             )
             .then((msg) =>
               setTimeout(() => {
-                if (!generativeText.value && client.customAdd.has(newID)) {
+                if (
+                  generativeText.value === false &&
+                  client.customAdd.has(newID)
+                ) {
                   msg.edit({ components: [addDisable] });
                   client.customAdd.delete(newID);
                 }
