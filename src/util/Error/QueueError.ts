919492a0e96d9e1e27bfdba83966bbf510f7c836
Nidrux <37250273+Nidrux@users.noreply.github.com>
2024-01-23 16:33:31 +0100

    feat: add sentry

--- src/util/Error/QueueError.ts
+++ src/util/Error/QueueError.ts
@@ -4,14 +4,16 @@
 export default class QueueError extends Error {
   public readonly context?: globalThis.CanJSON;
   public readonly id?: string;
+  public readonly guildId?: string;
   constructor(
     message: string,
-    options: { error?: Error; id?: string; context?: globalThis.CanJSON } = {},
+    options: { error?: Error; id?: string; guildId? : string; context?: globalThis.CanJSON } = {},
   ) {
     const { error, id, context } = options;
     super(message, { cause: error });
     this.name = this.constructor.name;
     this.context = context;
     this.id = id;
+    this.guildId = this.guildId;
   }
 }
