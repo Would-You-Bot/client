cffddfcc88bda7f087260278eee3ea85eef5a624
mezotv <mezotv@users.noreply.github.com>
2024-01-23 01:31:51 +0000

    chore(style): format files

--- src/util/webhookHandler.ts
+++ src/util/webhookHandler.ts
@@ -5,7 +5,7 @@ import {
   APIMessage,
 } from "discord.js";
 import { captureException } from "@sentry/node";
-import { Document, Model} from "mongoose";
+import { Document, Model } from "mongoose";
 import { IWebhookCache, WebhookCache } from "./Models/webhookCache";
 import WouldYou from "./wouldYou";
 import Cryptr from "cryptr";
@@ -38,78 +38,84 @@ export default class WebhookHandler {
       });
       try {
         const result = await this.send(webhookClient, content);
-        if(result.success) {
-          if(message.thread) {
+        if (result.success) {
+          if (message.thread) {
             const thread = await this.createThread(message, result.result);
             return thread;
           } else {
-            return result
+            return result;
           }
         } else {
           return result;
         }
       } catch (error) {
         const newWebhook = await this.webhookFallBack(channel, message);
-        if(newWebhook.success) {
+        if (newWebhook.success) {
           const result = await this.send(newWebhook.result, content);
-          if(result.success) {
-            if(message.thread) {
+          if (result.success) {
+            if (message.thread) {
               const thread = await this.createThread(message, result.result);
               return thread;
             } else {
-              return result
+              return result;
             }
           } else {
             return result;
           }
         } else {
-          return newWebhook
+          return newWebhook;
         }
       }
     } else {
       const newWebhook = await this.webhookFallBack(channel, message);
-      if(newWebhook.success) {
+      if (newWebhook.success) {
         const result = await this.send(newWebhook.result, content);
-        if(result.success) {
-          if(message.thread) {
+        if (result.success) {
+          if (message.thread) {
             const thread = await this.createThread(message, result.result);
             return thread;
           } else {
-            return result
+            return result;
           }
         } else {
           return result;
         }
       } else {
-        return newWebhook
+        return newWebhook;
       }
     }
   }
-  private async send(webhook: WebhookClient, content: any): Promise<Result<APIMessage>> {
-      const result = await webhook.send(content)
-      if(result) return {success: true, result: result}
-      return {success: false, error: new Error(`Failed to send webhook`)}
-
+  private async send(
+    webhook: WebhookClient,
+    content: any,
+  ): Promise<Result<APIMessage>> {
+    const result = await webhook.send(content);
+    if (result) return { success: true, result: result };
+    return { success: false, error: new Error(`Failed to send webhook`) };
   }
   private async webhookFallBack(
     channel: any,
-    message: IQueueMessage
+    message: IQueueMessage,
   ): Promise<Result<WebhookClient>> {
     const webhook = await this.createWebhook(
       channel,
       "Would You",
-      this.client.user?.avatarURL({extension: "png"}) as string,
+      this.client.user?.avatarURL({ extension: "png" }) as string,
       "Webhook token unavailable, creating new webhook",
     );
-    if(webhook.success) {
-      const result = await this.updateCache(channel.id, message.guildId ,webhook.result)
-      if(result.success) {
+    if (webhook.success) {
+      const result = await this.updateCache(
+        channel.id,
+        message.guildId,
+        webhook.result,
+      );
+      if (result.success) {
         return webhook;
       } else {
-        return {success: false, error: result.error}
+        return { success: false, error: result.error };
       }
     } else {
-      return webhook
+      return webhook;
     }
   }
   private async createWebhook(
@@ -123,62 +129,99 @@ export default class WebhookHandler {
         .permissionsFor(this.client.user)
         ?.has([PermissionFlagsBits.ManageWebhooks])
     )
-      return {success: false, error: new Error("No permissions to create a webhook")}
+      return {
+        success: false,
+        error: new Error("No permissions to create a webhook"),
+      };
     try {
-      const webhook = await channel
-           .createWebhook({
-             name: name ?? "Would You",
-             avatar: avatar ?? this.client.user?.displayAvatarURL(),
-             reason: reason ?? "Creating a webhook for the daily message system",
-           })
-           return {success: true, result: new WebhookClient({ id: webhook.id, token: webhook.token })};
+      const webhook = await channel.createWebhook({
+        name: name ?? "Would You",
+        avatar: avatar ?? this.client.user?.displayAvatarURL(),
+        reason: reason ?? "Creating a webhook for the daily message system",
+      });
+      return {
+        success: true,
+        result: new WebhookClient({ id: webhook.id, token: webhook.token }),
+      };
     } catch (error) {
-      return {success: false, error: error as Error}
+      return { success: false, error: error as Error };
     }
   }
-  private async updateCache(channelId: string, guildId: string,webhook: WebhookClient): Promise<Result<Document>> {
-      try {
-        const doc = await this.webhookModel.findOneAndUpdate({channelId: channelId}, {webhookToken: cryptr.encrypt(webhook.token), webhookId: webhook.id, guildId: guildId});
-        if(doc) {
-          return {success: true, result: doc}
-        } else {
-          const createdEntry = await this.createEntryInCache(channelId, guildId, webhook);
-          return createdEntry;
-        }
-      } catch (error) {
-        return {success: false, error: error as Error}
-      }
-  }
-  private async createEntryInCache(channelId: string, guildId: string, webhook: WebhookClient): Promise<Result<Document>> {
+  private async updateCache(
+    channelId: string,
+    guildId: string,
+    webhook: WebhookClient,
+  ): Promise<Result<Document>> {
     try {
-      const doc = await this.webhookModel.create({channelId: channelId, webhookId: webhook.id, webhookToken: cryptr.encrypt(webhook.token), lastUsageTimestamp: Date.now()})
-      if(doc) {
-        return {success: true, result: doc}
+      const doc = await this.webhookModel.findOneAndUpdate(
+        { channelId: channelId },
+        {
+          webhookToken: cryptr.encrypt(webhook.token),
+          webhookId: webhook.id,
+          guildId: guildId,
+        },
+      );
+      if (doc) {
+        return { success: true, result: doc };
       } else {
-        return {success: false, error: new Error("No new document could be created")}
+        const createdEntry = await this.createEntryInCache(
+          channelId,
+          guildId,
+          webhook,
+        );
+        return createdEntry;
       }
-    } catch(error) {
-      return {success: false, error: error as Error}
+    } catch (error) {
+      return { success: false, error: error as Error };
     }
   }
-  private async createThread(message: IQueueMessage, apiReturnValue: APIMessage): Promise<Result<any>> {
+  private async createEntryInCache(
+    channelId: string,
+    guildId: string,
+    webhook: WebhookClient,
+  ): Promise<Result<Document>> {
+    try {
+      const doc = await this.webhookModel.create({
+        channelId: channelId,
+        webhookId: webhook.id,
+        webhookToken: cryptr.encrypt(webhook.token),
+        lastUsageTimestamp: Date.now(),
+      });
+      if (doc) {
+        return { success: true, result: doc };
+      } else {
+        return {
+          success: false,
+          error: new Error("No new document could be created"),
+        };
+      }
+    } catch (error) {
+      return { success: false, error: error as Error };
+    }
+  }
+  private async createThread(
+    message: IQueueMessage,
+    apiReturnValue: APIMessage,
+  ): Promise<Result<any>> {
     const date = new Date();
-      try {
-
-        await this.client.rest.setToken(process.env.TOKEN as string);
-        await this.client.rest.post(`/channels/${message?.channelId}/messages/${apiReturnValue.id}/threads`, {
+    try {
+      await this.client.rest.setToken(process.env.TOKEN as string);
+      await this.client.rest.post(
+        `/channels/${message?.channelId}/messages/${apiReturnValue.id}/threads`,
+        {
           body: {
             name: `${[
               date.getFullYear(),
               date.getMonth() + 1,
               date.getDate(),
             ].join("/")} - Daily Message`,
-            auto_archive_duration: "1440"
-          }
-        })
-        return {success: true, result: "Thread created"}
-      } catch (error) {
-        return {success:false,error: error as Error}
-      }
+            auto_archive_duration: "1440",
+          },
+        },
+      );
+      return { success: true, result: "Thread created" };
+    } catch (error) {
+      return { success: false, error: error as Error };
+    }
   }
 }
