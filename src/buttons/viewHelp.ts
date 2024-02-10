696bdd20b6e0ab5b110b48d7e74a2b622a2e6081
Dominik K <mezo.dsn@gmail.com>
2024-02-10 02:55:52 +0100

    fix: do some small nav work

--- src/buttons/viewHelp.ts
+++ src/buttons/viewHelp.ts
@@ -13,7 +13,7 @@ const button: Button = {
     const helpembed = new EmbedBuilder()
       .setColor("#2b2d31")
       .setDescription(
-        "## Hello \n\n **Would You** is a bot that allows you to play the game 'Would You Rather' with your friends on Discord. \n\n## Main Game Modes \n\n </wouldyourather:1099463656124723305> \n\n## Privacy \n\n If you dont want to show up on leaderboards or votes. You can adjust your privacy settings using </privacy:1162855843658735716>. To find out what data we store visit our [Privacy Policy](https://wouldyoubot.gg/privacy)",
+        `## What is Would You? \n\n **Would You** is a bot that allows you to play the game 'Would You Rather' with your friends on Discord. \n## Main Game Modes \n The game modes of  ** <:roundyou:1009964111045607525> Would You** include </wouldyourather:1099463656124723305>, </neverhaveiever:1099463656124723303>, </whatwouldyoudo:1199732399270023310>, </higherlower:1153718305903890554> and last but not least there is </truth:1169062270068867217> or </dare:1169062270068867215> \n## Privacy \n If you dont want to show up on leaderboards or votes. You can adjust your privacy settings using </privacy:1162855843658735716>. To find out what data we store visit our [Privacy Policy](https://wouldyoubot.gg/privacy)`,
       );
 
     const button =
--- src/commands/utility/help.ts
+++ src/commands/utility/help.ts
@@ -25,21 +25,10 @@ const command: ChatInputCommand = {
    * @param {guildModel} guildDb
    */
   execute: async (interaction, client, guildDb) => {
-    const languageMappings = {
-      de_DE: "de",
-      en_EN: "en",
-      es_ES: "es",
-      fr_FR: "fr",
-    } as any;
-
-    const commands = await client.application?.commands.fetch({
-      withLocalizations: true,
-    });
-    const type = languageMappings[guildDb?.language] || "en";
     const helpembed = new EmbedBuilder()
       .setColor("#2b2d31")
       .setDescription(
-        `## Hello \n\n **Would You** is a bot that allows you to play the game 'Would You Rather' with your friends on Discord. \n\n## Main Game Modes \n\n </wouldyourather:1099463656124723305> \n\n## Privacy \n\n If you dont want to show up on leaderboards or votes. You can adjust your privacy settings using </privacy:1162855843658735716>. To find out what data we store visit our [Privacy Policy](https://wouldyoubot.gg/privacy)`,
+        `## What is Would You? \n\n **Would You** is a bot that allows you to play the game 'Would You Rather' with your friends on Discord. \n## Main Game Modes \n The game modes of  ** <:roundyou:1009964111045607525> Would You** include </wouldyourather:1099463656124723305>, </neverhaveiever:1099463656124723303>, </whatwouldyoudo:1199732399270023310>, </higherlower:1153718305903890554> and last but not least there is </truth:1169062270068867217> or </dare:1169062270068867215> \n## Configure the bot \n  \n## Privacy \n If you dont want to show up on leaderboards or votes. You can adjust your privacy settings using </privacy:1162855843658735716>. To find out what data we store visit our [Privacy Policy](https://wouldyoubot.gg/privacy)`,
       );
 
     const button =
