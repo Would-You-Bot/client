238416ca7bf522ea7e26a4635a2db772343ddeda
mezotv <mezotv@users.noreply.github.com>
2024-02-19 01:28:01 +0000

    chore(style): format files

--- src/util/Models/ShardClusterStore.ts
+++ src/util/Models/ShardClusterStore.ts
@@ -1,33 +1,32 @@
 import { Schema, model } from "mongoose";
 export interface IShardClusterStore {
-    shard: number
-    cluster: number
-    isMaster: boolean
-    pid: number
+  shard: number;
+  cluster: number;
+  isMaster: boolean;
+  pid: number;
 }
 
 const shardClusterStoreSchema = new Schema({
-    shard: {
-        type: Number,
-        require: true,
-        unique: true
-    },
-    cluster: {
-        type: Number,
-        require: true
-    },
-    isMaster: {
-        type: Boolean,
-        default: false,
-    },
-    pid: {
-        type: Number,
-        default: 0
-    }
+  shard: {
+    type: Number,
+    require: true,
+    unique: true,
+  },
+  cluster: {
+    type: Number,
+    require: true,
+  },
+  isMaster: {
+    type: Boolean,
+    default: false,
+  },
+  pid: {
+    type: Number,
+    default: 0,
+  },
 });
 
-export const shardClusterStoreModel = model<IShardClusterStore>("shardclusterstore", shardClusterStoreSchema)
-
-
-
-
+export const shardClusterStoreModel = model<IShardClusterStore>(
+  "shardclusterstore",
+  shardClusterStoreSchema,
+);
