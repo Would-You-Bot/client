57375c78b22c9f2d22c846593d39281d2e984666
Nidrux <37250273+Nidrux@users.noreply.github.com>
2024-03-14 22:26:00 +0100

    fix: decouple

--- src/cluster.ts
+++ src/cluster.ts
@@ -9,28 +9,14 @@ const manager = new ClusterManager(`${__dirname}/index.js`, {
   token: process.env.TOKEN,
 });
 
-const socket = new net.Socket();
-
 manager.on("clusterCreate", (cluster) => {
   cluster.on("ready", () => {
     console.log(`[Cluster Manager] Cluster ${cluster.id} ready`);
   });
-
   cluster.on("reconnecting", () => {
     console.log(`[Cluster Manager] Cluster ${cluster.id} reconnecting`);
   });
   console.log(`[Cluster Manager] Cluster ${cluster.id} created`);
-
-  let host = process.env.TCP_HOST || "dailyMessage";
-  let port = parseInt(process.env.TCP_PORT ? process.env.TCP_PORT : "8989");
-  socket.connect({ host: host, port: port }, () => {
-    console.log("connected");
-    socket.write(`${process.env.TCP_KEY}#CREATE#${cluster.id}`);
-  });
-  socket.on("data", (data) => {
-    console.log(data);
-    socket.destroy();
-  });
 });
 
 manager.spawn({ timeout: -1 });
