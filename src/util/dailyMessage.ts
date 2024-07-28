fb70854b249519a14fa5d9e6b3fb8ae6cb0a6a4b
ForGetFulSkyBro <forgetfulskybro9@gmail.com>
2024-07-28 19:01:33 -0400

    fix: QOTD Mentions (real)

--- src/util/dailyMessage.ts
+++ src/util/dailyMessage.ts
@@ -171,7 +171,7 @@ export default class DailyMessage {
         {
           embeds: [embed],
           content: message.role ? `<@&${message.role}>` : undefined,
-          allowedMentions: { roles: message.role ? [message.role] : undefined },
+          allowedMentions: message.role ? { roles: [message.role] } : undefined,
           avatarURL:
             message.webhook.avatar ||
             this.client.user?.displayAvatarURL({ forceStatic: false }),
