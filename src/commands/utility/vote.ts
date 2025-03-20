08a5124b1dc67231be71d023b1e2959bdfe89e43
mezotv <mezotv@users.noreply.github.com>
2025-03-20 02:24:00 +0000

    chore(style): format files

--- src/commands/utility/vote.ts
+++ src/commands/utility/vote.ts
@@ -24,20 +24,24 @@ const command: ChatInputCommand = {
     }),
 
   execute: async (interaction, client, guildDb) => {
-    const { topgg } = client.config.emojis.vote
-    
+    const { topgg } = client.config.emojis.vote;
+
     const voteEmbed = new EmbedBuilder()
       .setAuthor({
         name: client.translation.get(guildDb?.language, "Vote.embed.name"),
         iconURL: `https://cdn.discordapp.com/emojis/${topgg}.webp?size=512&quality=lossless`,
       })
       .setColor("#FF3366")
-      .setDescription(client.translation.get(guildDb?.language, "Vote.embed.description"),)
+      .setDescription(
+        client.translation.get(guildDb?.language, "Vote.embed.description"),
+      );
 
     const button =
       new ActionRowBuilder<MessageActionRowComponentBuilder>().addComponents(
         new ButtonBuilder()
-          .setLabel(client.translation.get(guildDb?.language, "Vote.button.label"),)
+          .setLabel(
+            client.translation.get(guildDb?.language, "Vote.button.label"),
+          )
           .setStyle(5)
           .setEmoji(topgg)
           .setURL("https://top.gg/bot/981649513427111957/vote/"),
