5b2936fb6dd99255783b8763c035a6b742a227e7
mezotv <mezotv@users.noreply.github.com>
2025-10-14 02:24:59 +0000

    chore(style): format files

--- src/buttons/utilityInformation/voting.ts
+++ src/buttons/utilityInformation/voting.ts
@@ -1,4 +1,9 @@
-import { ActionRowBuilder, AnyComponentBuilder, ButtonBuilder, ButtonComponentData } from "discord.js";
+import {
+  ActionRowBuilder,
+  AnyComponentBuilder,
+  ButtonBuilder,
+  ButtonComponentData,
+} from "discord.js";
 import type { Button } from "../../interfaces";
 
 const button: Button = {
@@ -33,21 +38,25 @@ const button: Button = {
     } else {
       replyContent;
     }
-    const unchangedRow = ActionRowBuilder.from(interaction.message.components[1])
-    const updatedResult = ActionRowBuilder.from(interaction.message.components[0])
-    
+    const unchangedRow = ActionRowBuilder.from(
+      interaction.message.components[1],
+    );
+    const updatedResult = ActionRowBuilder.from(
+      interaction.message.components[0],
+    );
+
     const resultButton = updatedResult.components[0] as ButtonBuilder;
     resultButton.setDisabled(false);
 
     // @ts-expect-error no clue why it complains but it works!
     await interaction.update({
-      components: [updatedResult, unchangedRow]
-    })
+      components: [updatedResult, unchangedRow],
+    });
 
-   interaction.followUp({
-     content: replyContent,
-     ephemeral: true,
-   });
+    interaction.followUp({
+      content: replyContent,
+      ephemeral: true,
+    });
   },
 };
 
